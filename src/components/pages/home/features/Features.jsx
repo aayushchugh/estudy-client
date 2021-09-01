6d80a74d9dce501962982d711bf2f8d681b6ae57
Ayush Chugh <ayushchugh2006@gmail.com>
2021-09-01 10:33:16 +0530

    converted tailwind to css

--- src/components/pages/home/features/Features.jsx
+++ src/components/pages/home/features/Features.jsx
@@ -4,12 +4,12 @@ import { BPrimary } from '../../../UiComponents/Btn';
 
 export default function Features() {
 	return (
-		<section>
-			<div>
+		<section className='feature-section' >
+			<div className='feature-section__content'>
 				<HPrimary>What We Provide</HPrimary>
 				<BPrimary>Explore &rarr;</BPrimary>
 			</div>
-			<div>
+			<div className='feature-section__img' >
 				<img src='/images/illustrations/features.svg' alt='background' />
 			</div>
 		</section>

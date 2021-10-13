ec9b54170603402f5ebb88a47f954089f66a084d
Ayush Chugh <ayushchugh2006@gmail.com>
2021-10-13 11:34:09 +0530

    render class using api on home page

--- src/components/pages/home/class/Class.jsx
+++ src/components/pages/home/class/Class.jsx
@@ -1,32 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import { HPrimary } from '../../../UiComponents/Typography';
 import { CardHeading } from '../../../UiComponents/Card';
 import './class.scss';
 
 function Class() {
+	const [response, setResponse] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get(
+				`${process.env.REACT_APP_API_URL}/class/get-all?auth=${process.env.REACT_APP_API_AUTH}`
+			)
+			.then(data => {
+				setResponse(data.data.data);
+			});
+	}, []);
+
 	return (
 		<section className='class-section' id='class-section'>
 			<HPrimary className='class-section__heading'>Study Material For</HPrimary>
 
 			<div className='class-section__class'>
-				<CardHeading
-					className='class-section__card'
-					heading='class 9'
-					para='9thie? Just entered the world of Physics, Chemistry and Bio from Science? We got you covered!'
-				/>
-
-				<CardHeading
-					className='class-section__card'
-					heading='class 10'
-					para='Is saal pehli boards dunga, tuadi preparation preparation, saadi preparation halwa?'
-				/>
-
-				<CardHeading
-					className='class-section__card'
-					heading='class 11'
-					para="We understand this class, so we won't talk much about it. We hope you get that. :')"
-				/>
+				{response.map(data => (
+					<CardHeading
+						className='class-section__card'
+						heading={data.title}
+						para={data.description}
+					/>
+				))}
 			</div>
 		</section>
 	);

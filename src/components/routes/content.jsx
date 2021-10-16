4e269076a2f3cb97e562be2c083dadbe4822bd30
Ayush Chugh <ayushchugh2006@gmail.com>
2021-10-16 15:10:52 +0530

    redirect to login page if not loged in while visiting class page

--- src/components/routes/content.jsx
+++ src/components/routes/content.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { default as jwt } from 'jsonwebtoken';
+
 import Notes from '../pages/content/notes/Notes';
 import Pyq from '../pages/content/pyq/Pyq';
 import SamplePaper from '../pages/content/sample_paper/SamplePaper';
 
 function Content() {
+	const token = localStorage.getItem('token');
+	const user = jwt.decode(token);
+
+	if (!user) window.location = '/login';
+
 	return (
 		<main>
 			<Notes />
